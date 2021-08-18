00b7f6e4aa3b68fc1119c1ae66f07339e7fc3f53
gusdnvkfks <69062156+gusdnvkfks@users.noreply.github.com>
2021-08-18 09:36:59 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+export function objToArr(obj) {
+  // TODO: objToArr(obj)의 리턴 배열은 arr와 같은 요소를 가지고 있어야 한다.
+}
+
+export function arrToObj(arr) {
+  // TODO: arrToObj(arr)의 리턴 객체는 obj와 같은 키와 속성을 가지고 있어야 한다.
+}
+
+export function objArrToArrArr(objArr) {
+  // TODO: objArrToArrArr([obj, obj, obj])의 리턴 배열은 [arr, arr, arr]와 같은 요소를 가지고 있어야 한다.
+}
